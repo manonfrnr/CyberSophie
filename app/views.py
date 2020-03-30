6014cca19040f71715066ad22b315de1ea37124e
manonfrnr <fournier@et.esiea.fr>
2020-03-30 20:31:54 +0200

    [ADD] Affichage du sondage

--- app/views.py
+++ app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def sondage(request):
+    return render(request, 'app/sondage.html', {'questions': Question.objects.all(), "choix": Choix.objects.all()})
