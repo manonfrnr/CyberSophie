33517f293c209b3ef4d61a6b517f05d930e72386
manonfrnr <fournier@et.esiea.fr>
2020-03-30 19:03:24 +0200

    [ADD] Models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-class
+class Question(models.Model):
+    enonce = models.CharField(max_length=300)
+    isLibre = models.BooleanField()
+
+
+class Choix(models.Model):
+    question = models.ForeignKey('Question', on_delete=models.CASCADE)
+    valeur = models.CharField(max_length=100)
+
+class Reponse(models.Model):
+    question = models.ForeignKey('Question', on_delete=models.CASCADE)
+    valeur = models.CharField(max_length=1000)
